698e591dd25d26a989e0f1a69862520ebb2be948
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-08 12:52:29 +0100

    add story

--- styles/components/Card.stories.js
+++ styles/components/Card.stories.js
@@ -1,10 +1,19 @@
+import DefaultHtml from "./styles/components/Card.default.html";
+
+export const Deeeefault = () => DefaultHtml;
+
 export default {
   title: "Card",
 };
 
-export const Default = () => /*html */ `
-<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+export const Default = () => /*html*/ `
+<article class="Card">
+  <h2 class="Card__title">Frage 1</h2>
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
 sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
 At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
-At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>`;
+At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+  </p>
+</article>
+`;
