3a9f9fea843df5f2ced667b9fe05b2d310412c9c
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-08 17:58:06 +0100

    rework navbar

--- style/components/Card.stories.js
+++ style/components/Card.stories.js
@@ -12,11 +12,31 @@ Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
 At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
   </p>
   <button type="button">Show Answer</button>
-  <ul>
-    <li>Tag 1</li>
-    <li>Tag 2</li>
-    <li>Tag 3</li>
-    <li>Tag 4</li>
-  </ul>
+  <div class="Card__tag__wrapper">
+  <p class="Card__tags">Tag 1</p>
+  <p class="Card__tags">Tag 2</p>
+  <p class="Card__tags">Tag 3</p>
+  <p class="Card__tags">Tag 4</p>
+</div>
+</article>
+`;
+
+export const HideAnswer = () => /* html */ `
+<article class="Card">
+  <h2 class="Card__title">Frage 1</h2>
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+  </p>
+  <button type="button">Hide Answer</button>
+  <p>Unheimlich gute Antwort, die alle Fragen auf einmal klärt</p>
+    <div class="Card__tag__wrapper">
+      <p class="Card__tags">Tag 1</p>
+      <p class="Card__tags">Tag 2</p>
+      <p class="Card__tags">Tag 3</p>
+      <p class="Card__tags">Tag 4</p>
+    </div>
 </article>
 `;
