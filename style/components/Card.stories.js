5b4e0a65c4d6b8424343f447a0cc3f4cc4b063a3
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-08 15:41:24 +0100

    extend first story

--- style/components/Card.stories.js
+++ style/components/Card.stories.js
@@ -11,5 +11,12 @@ At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergr
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
 At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
   </p>
+  <button type="button">Show Answer</button>
+  <ul>
+    <li>Tag 1</li>
+    <li>Tag 2</li>
+    <li>Tag 3</li>
+    <li>Tag 4</li>
+  </ul>
 </article>
 `;
