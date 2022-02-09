654a35d12489d1487b567118acf5cceb4aeebee4
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-09 16:10:21 +0100

    add bookmark

--- style/components/Card.stories.js
+++ style/components/Card.stories.js
@@ -5,6 +5,7 @@ export default {
 export const Default = () => /*html*/ `
 <article class="Card">
   <h2 class="Card__title">Frage 1</h2>
+  <p class="Card__bookmark"></p>
   <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
 sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
 At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
@@ -24,6 +25,28 @@ At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergr
 export const HideAnswer = () => /* html */ `
 <article class="Card">
   <h2 class="Card__title">Frage 1</h2>
+  <p class="Card__bookmark"></p>
+  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+  </p>
+  <button type="button">Hide Answer</button>
+  <p>Unheimlich gute Antwort, die alle Fragen auf einmal klärt</p>
+    <div class="Card__tag__wrapper">
+      <p class="Card__tags">Tag 1</p>
+      <p class="Card__tags">Tag 2</p>
+      <p class="Card__tags">Tag 3</p>
+      <p class="Card__tags">Tag 4</p>
+    </div>
+</article>
+`;
+
+export const Bookmarked = () => /* html */ `
+<article class="Card">
+  <h2 class="Card__title">Frage 1</h2>
+  <p class="Card__bookmark--active"></p>
   <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
 sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
 At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
