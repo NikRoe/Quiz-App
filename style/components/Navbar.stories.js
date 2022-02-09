69ba56372fd002157c0015c103644660c5ea93d3
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-09 09:07:46 +0100

    rename highlight to active

--- style/components/Navbar.stories.js
+++ style/components/Navbar.stories.js
@@ -9,9 +9,8 @@ export const Navbar = () => /*html*/ `<footer class="Navbar Navbar__wrapper">
 <a class="Navbar__content">Profile</a>
 </footer>`;
 
-export const Highlight =
-  () => /*html */ `<footer class="Navbar Navbar__wrapper">
-<a class="Navbar__content Navbar--highlight">Frontpage</a>
+export const Active = () => /*html */ `<footer class="Navbar Navbar__wrapper">
+<a class="Navbar__content Navbar--active">Frontpage</a>
 <a class="Navbar__content">Bookmarks</a>
 <a class="Navbar__content">Notes</a>
 <a class="Navbar__content">Profile</a>
