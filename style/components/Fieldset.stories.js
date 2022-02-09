c8f18db4694202dd380acb9d78260cba8247766a
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-09 19:40:07 +0100

    add button story

--- style/components/Fieldset.stories.js
+++ style/components/Fieldset.stories.js
@@ -5,17 +5,17 @@ export default {
 export const Fieldset = () => /* html */ `
 <form class="Form">
     <fieldset class="Form__fieldset">
-        <label for="question-input" class="Textarea">Frage</label>
+        <label for="question-input" class="Form__label">Frage</label>
         <textarea name="question-input" id="question-input" placeholder="Enter your question" rows="10"></textarea>
     </fieldset>
 
     <fieldset class="Form__fieldset">
-        <label for="answer-input" class="Textarea">Antwort</label>
+        <label for="answer-input" class="Form__label">Antwort</label>
         <textarea name="answer-input" id="answer-input" placeholder="Enter the answer" rows="10"></textarea>
     </fieldset>
 
     <fieldset class="Form__fieldset">
-        <label for="tag-input" class="Textarea">Tags</label>
+        <label for="tag-input" class="Form__label">Tags</label>
         <textarea name="tag-input" id="tag-input" placeholder="Add some tags. Seperate by comma" rows="10"></textarea>
     </fieldset>
 </form>`;
