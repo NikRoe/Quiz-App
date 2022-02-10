6826ca099d1af051919e4cd7eeb769ea8bb1d6bc
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-10 09:34:23 +0100

    add svgs to navbar

--- style/components/Navbar.stories.js
+++ style/components/Navbar.stories.js
@@ -2,16 +2,58 @@ export default {
   title: "Navbar",
 };
 
-export const Navbar = () => /*html*/ `<footer class="Navbar Navbar__wrapper">
-<a class="Navbar__content">Frontpage</a>
-<a class="Navbar__content">Bookmarks</a>
-<a class="Navbar__content">Notes</a>
-<a class="Navbar__content">Profile</a>
+export const Navbar = () => /*html*/ `    <footer class="Navbar__wrapper">
+<a class="Navbar__content" href="index.html"
+  ><img
+    src="assets/home-svgrepo-com.svg"
+    class="Navbar__images"
+    alt="navigate to home"
+/></a>
+<a class="Navbar__content" href="bookmarks.html"
+  ><img
+    src="assets/bookmark-alt-svgrepo-com.svg"
+    class="Navbar__images"
+    alt="navigate to bookmarks"
+/></a>
+<a class="Navbar__content" href="create.html"
+  ><img
+    src="assets/note-svgrepo-com.svg"
+    class="Navbar__images"
+    alt="navigate to create page"
+/></a>
+
+<a class="Navbar__content"
+  ><img
+    src="assets/profile-svgrepo-com.svg"
+    class="Navbar__images"
+    alt="navigate to profile page"
+/></a>
 </footer>`;
 
-export const Active = () => /*html */ `<footer class="Navbar Navbar__wrapper">
-<a class="Navbar__content Navbar--active">Frontpage</a>
-<a class="Navbar__content">Bookmarks</a>
-<a class="Navbar__content">Notes</a>
-<a class="Navbar__content">Profile</a>
+export const Active = () => /*html */ `    <footer class="Navbar__wrapper">
+<a class="Navbar__content" href="index.html"
+  ><img
+    src="assets/home-svgrepo-com.svg"
+    class="Navbar__images"
+    alt="navigate to home"
+/></a>
+<a class="Navbar__content" href="bookmarks.html"
+  ><img
+    src="assets/bookmark-alt-svgrepo-com.svg"
+    class="Navbar__images"
+    alt="navigate to bookmarks"
+/></a>
+<a class="Navbar__content" href="create.html"
+  ><img
+    src="assets/note-svgrepo-com.svg"
+    class="Navbar__images"
+    alt="navigate to create page"
+/></a>
+
+<a class="Navbar__content Navbar--active"
+  ><img
+    src="assets/profile-svgrepo-com.svg"
+    class="Navbar__images"
+    alt="navigate to profile page"
+/></a>
 </footer>`;
