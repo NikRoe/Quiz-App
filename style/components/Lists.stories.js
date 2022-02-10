d88f9965a7c1ef74308b547b2dcbe367b792a038
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-10 17:01:36 +0100

    add profile story

---
+++ style/components/Lists.stories.js
@@ -0,0 +1,15 @@
+export default {
+  title: "Lists",
+};
+
+export const Listdefault = () => /* html */ `      <article class="Card">
+<h2>Your skill tags</h2>
+<ul class="List__default">
+  <li class="List__tag">#HTML</li>
+  <li class="List__tag">#CSS</li>
+  <li class="List__tag">#Git</li>
+  <li class="List__tag">#GitHub</li>
+  <li class="List__tag">#etc.</li>
+</ul>
+</article>
+`;
