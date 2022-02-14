92a1c16f2beb720e227990bc2f0b0eb34c52967e
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-14 17:47:47 +0100

    add script.js and interactive button

---
+++ script.js
@@ -0,0 +1,9 @@
+/* define vars for the button (showAnswer) and the answer (firstAnswer)
+by default, the firstAnswer is hidden on opening the page and can be toggled */
+
+const showAnswer = document.querySelector('[data-js="first-button"]');
+const firstAnswer = document.querySelector('[data-js="first-answer"]');
+
+showAnswer.addEventListener("click", () => {
+  firstAnswer.classList.toggle("display-toggle");
+});
--- style/components/Card.stories.js
+++ style/components/Card.stories.js
@@ -16,13 +16,18 @@ At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergr
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
 At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
   </p>
-  <button type="button" class="Button__default">Show Answer</button>
+  <button type="button" class="Button__default" data-js="first-button">
+  Show Answer
+  </button>
+  <p class="display-toggle" data-js="first-answer">
+    Unheimlich gute Antwort, die keine Fragen offen lässt.
+  </p>
   <div class="Card__tag__wrapper">
-  <p class="Card__tags">Tag 1</p>
-  <p class="Card__tags">Tag 2</p>
-  <p class="Card__tags">Tag 3</p>
-  <p class="Card__tags">Tag 4</p>
-</div>
+    <p class="Card__tags">Tag 1</p>
+    <p class="Card__tags">Tag 2</p>
+    <p class="Card__tags">Tag 3</p>
+    <p class="Card__tags">Tag 4</p>
+  </div>
 </article>
 `;
 
