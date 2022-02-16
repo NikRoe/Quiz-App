bfc4501aa62b324e1756714817de8cd57636678e
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-16 18:12:44 +0100

    add input counter

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ showAnswer.addEventListener("click", () => {
     showAnswer.textContent = "Hide answer";
   }
 });
+
+/* define vars and event for getting a counter of how many symbols
+ have been put into the textarea of the form */
+
+const textfieldInput = document.querySelector('[data-js="input-area"]');
+const textfieldCounter = document.querySelector('[data-js="input-counter"]');
+
+textfieldInput.addEventListener("input", () => {
+  textfieldCounter.textContent = textfieldInput.value.length;
+});
