81d615ada6b9a41decf303730d54070b0f00ba1e
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-17 17:48:17 +0100

    replace navbar toggle with add

--- script.js
+++ script.js
@@ -56,3 +56,57 @@ bookmarkImage?.addEventListener("click", () => {
 });
 
 console.log(bookmarkImage.src);
+
+const indexPage = document.querySelector('[data-js="index-Page"]');
+const bookmarksPage = document.querySelector('[data-js="bookmarks-Page"]');
+const createPage = document.querySelector('[data-js="create-Page"]');
+const profilePage = document.querySelector('[data-js="profile-Page"]');
+
+const indexNavbar = document.querySelector('[data-js="index-navbar"]');
+const bookmarksNavbar = document.querySelector('[data-js="bookmarks-navbar"]');
+const createNavbar = document.querySelector('[data-js="create-navbar"]');
+const profileNavbar = document.querySelector('[data-js="profile-navbar"]');
+
+indexNavbar?.addEventListener("click", () => {
+  indexNavbar.classList.add("Navbar--active");
+  bookmarksNavbar.classList.remove("Navbar--active");
+  createNavbar.classList.remove("Navbar--active");
+  profileNavbar.classList.remove("Navbar--active");
+  indexPage.classList.remove("display-toggle");
+  bookmarksPage.classList.add("display-toggle");
+  createPage.classList.add("display-toggle");
+  profilePage.classList.add("display-toggle");
+});
+
+bookmarksNavbar?.addEventListener("click", () => {
+  bookmarksNavbar.classList.add("Navbar--active");
+  indexNavbar.classList.remove("Navbar--active");
+  createNavbar.classList.remove("Navbar--active");
+  profileNavbar.classList.remove("Navbar--active");
+  bookmarksPage.classList.remove("display-toggle");
+  indexPage.classList.add("display-toggle");
+  createPage.classList.add("display-toggle");
+  profilePage.classList.add("display-toggle");
+});
+
+createNavbar?.addEventListener("click", () => {
+  createNavbar.classList.add("Navbar--active");
+  indexNavbar.classList.remove("Navbar--active");
+  bookmarksNavbar.classList.remove("Navbar--active");
+  profileNavbar.classList.remove("Navbar--active");
+  createPage.classList.remove("display-toggle");
+  bookmarksPage.classList.add("display-toggle");
+  indexPage.classList.add("display-toggle");
+  profilePage.classList.add("display-toggle");
+});
+
+profileNavbar?.addEventListener("click", () => {
+  profileNavbar.classList.add("Navbar--active");
+  indexNavbar.classList.remove("Navbar--active");
+  bookmarksNavbar.classList.remove("Navbar--active");
+  createNavbar.classList.remove("Navbar--active");
+  profilePage.classList.remove("display-toggle");
+  bookmarksPage.classList.add("display-toggle");
+  indexPage.classList.add("display-toggle");
+  createPage.classList.add("display-toggle");
+});
