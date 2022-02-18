297d25acf94dc3340db0b2d79390f8491e615c96
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-18 14:14:10 +0100

    change import links

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-import * as randomName from "./style/components/JavaScript-Components/Card.js";
-import * as secondRandomName from "./style/components/JavaScript-Components/Textfields.js";
-import * as thirdRandomName from "./style/components/JavaScript-Components/Navbar.js";
-
-console.log(randomName.bookmarkIcons);
-console.log(secondRandomName.textFields);
-console.log(thirdRandomName.indexPage);
+import "./style/components/JavaScript-Components/Card.js";
+import "./style/components/JavaScript-Components/Textfields.js";
+import "./style/components/JavaScript-Components/Navbar.js";
