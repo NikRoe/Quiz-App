6a3f11a31a9abcceba362d3493a2fe28c7911ae7
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-18 14:22:03 +0100

    add utils.css

--- style/components/Card.stories.js
+++ style/components/Card.stories.js
@@ -6,22 +6,34 @@ export const Default = () => /*html*/ `
 <article class="Card">
   <h2 class="Card__title">Frage 1</h2>
   <img
-  src="../../assets/bookmars-svgrepo-com.svg"
-  alt="unchecked bookmark"
-  class="Card__bookmark"
-/>
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
-sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
-At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
-At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    src="assets/bookmark-svgrepo-com.svg"
+    alt="unchecked bookmark"
+    class="Card__bookmark"
+    data-js="bookmark-image"
+  />
+  <p>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+    erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+    et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+    Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+    et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+    accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+    no sea takimata sanctus est Lorem ipsum dolor sit amet.
   </p>
-  <button type="button" class="Button__default" data-js="first-button">
-  Show Answer
-  </button>
-  <p class="display-toggle" data-js="first-answer">
-    Unheimlich gute Antwort, die keine Fragen offen lässt.
-  </p>
+  <div data-js="answer-container">
+    <button
+      type="button"
+      class="Button__default"
+      data-js="answer-button"
+    >
+      Show Answer
+    </button>
+    <p class="display-toggle" data-js="answer">
+      Unheimlich gute Antwort, die keine Fragen offen lässt.
+    </p>
+  </div>
   <div class="Card__tag__wrapper">
     <p class="Card__tags">Tag 1</p>
     <p class="Card__tags">Tag 2</p>
